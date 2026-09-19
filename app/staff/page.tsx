ece69daf7ea00528206c7371b38f20d6ac546d5d
FPP3rd <sho.furu.0609@gmail.com>
2026-09-19 23:27:58 +0900

    Restore original announcement management

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -517,7 +517,7 @@ export default function StaffPage() {
             className="staffMenuCard"
             onClick={() =>
               router.push(
-                "/staff/notices"
+                "/staff/announcements"
               )
             }
           >
